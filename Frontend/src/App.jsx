91a8e03a3c7eedaeb5251de6f5e6def68b03cceb
piyush <piyushujgaokar544@gmail.com>
2026-04-09 20:36:09 +0530

    Set Up Redux and store user data

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Auth from './pages/Auth'
-import axios from 'axios'
-import { useEffect } from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUserData } from "./redux/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    
- try {
-       const getUser=async()=>{
-      const result=await axios.get('http://localhost:3000/api/user/current-user',{
-        withCredentials:true
-      })
-      console.log(result.data)
-    }
-
-  getUser()
- } catch (error) {
-    console.log(error)
- }  
-  })
-
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const result = await axios.get(
+          "http://localhost:3000/api/user/current-user",
+          {
+            withCredentials: true,
+          },
+        );
+        dispatch(setUserData(result.data));
+      } catch (error) {
+        console.log(error);
+        dispatch(setUserData(null));
+      }
+    };
+    getUser();
+  }, []);
 
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/auth' element={<Auth/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
